8da832845479c83d36e94a536a8058b559490da9
fyj <fyj@nwafu.edu.cn>
2024-04-14 22:28:15 +0800

    tabbar-vant

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,67 +1,44 @@
 <script setup>
   import { ref } from 'vue'
-  import { useRouter } from 'vue-router'
   import tabbarData from '@/assets/data/tabbar'
   import { getAssetURL } from '@/utils/load_assets.js'
 
   const curIndex = ref(0)
-  const router = useRouter()
-  const itemClick = (index, item) => {
-    curIndex.value = index
-    router.push(item.path)
-  }
 </script>
 
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabbarData" :key="item.text">
-      <div class="tab-bar-item" @click="itemClick(index, item)">
-        <div class="icon">
-          <img
-            v-if="curIndex !== index"
-            :src="getAssetURL(item.image)"
-            alt=""
-          />
-          <img v-else :src="getAssetURL(item.imageActive)" alt="" />
-        </div>
-        <div class="text" :class="{ active: curIndex === index }">
-          {{ item.text }}
-        </div>
-      </div>
-    </template>
+    <van-tabbar v-model="curIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData" :key="item.text">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img
+              v-if="curIndex !== index"
+              :src="getAssetURL(item.image)"
+              alt=""
+            />
+            <img v-else :src="getAssetURL(item.imageActive)" alt="" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
 <style lang="less" scoped>
   .tab-bar {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 50px;
-    display: flex;
+    // 局部定义一个变量: 只针对.tab-bar子元素才生效
+    // --van-tabbar-item-icon-size: 30px !important;
 
-    border-top: 1px solid #f3f3f3;
+    // 找到类, 对类中的CSS属性重写
+    // :deep(.class)找到子组件的类, 让子组件的类也可以生效
+    // :deep(.van-tabbar-item__icon) {
+    //   font-size: 50px;
+    // }
 
-    .tab-bar-item {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-
-      img {
-        width: 36px;
-      }
-
-      .text {
-        font-size: 12px;
-        margin-top: 2px;
-
-        &.active {
-          color: var(--primary-color);
-        }
-      }
+    img {
+      height: 26px;
     }
   }
 </style>
