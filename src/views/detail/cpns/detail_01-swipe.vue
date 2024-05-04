9d6dae57e191b40593399f9e212b60f8ce2d7ff9
fyj <fyj@nwafu.edu.cn>
2024-05-04 20:37:21 +0800

    detail-swipe-indicator

--- src/views/detail/cpns/detail_01-swipe.vue
+++ src/views/detail/cpns/detail_01-swipe.vue
@@ -4,6 +4,23 @@
   const detailStore = useDetailStore()
   const housePics =
     detailStore.houseDetail.mainPart.topModule.housePicture.housePics
+  // 对数据进行分类
+  const res = {}
+  const getKey = (title) => {
+    const group = title.match(/【(.*?)】/i)
+    return group[1]
+  }
+  housePics.forEach((item) => {
+    const key = getKey(item.title)
+    if (!res[key]) res[key] = []
+    res[key].push(item)
+  })
+  // 对当前轮播图序号进行自定义处理
+  const getActive = (active, key) => {
+    const housePic = housePics[active]
+    const arr = res[key]
+    return arr.findIndex((item) => item === housePic) + 1
+  }
 </script>
 
 <template>
@@ -14,8 +31,17 @@
           <img :src="item.url" alt="" />
         </van-swipe-item>
       </template>
-      <template #indicator="{ active, total }">
-        <div class="indicator">{{ active + 1 }}/{{ total }}</div>
+      <template #indicator="{ active }">
+        <div class="indicator">
+          <template v-for="(value, key) in res" :key="key">
+            <span class="item" :class="{ active: getActive(active, key) }">
+              <span>{{ key }}</span>
+              <span v-if="getActive(active, key)">
+                {{ getActive(active, key) }}/{{ value.length }}
+              </span>
+            </span>
+          </template>
+        </div>
       </template>
     </van-swipe>
   </div>
@@ -31,11 +57,22 @@
       position: absolute;
       right: 5px;
       bottom: 5px;
-      display: flex;
       padding: 2px 5px;
       font-size: 12px;
       color: #fff;
       background: rgba(0, 0, 0, 0.8);
+      display: flex;
+
+      .item {
+        margin: 0 3px;
+
+        &.active {
+          padding: 0 3px;
+          border-radius: 5px;
+          background-color: #fff;
+          color: #333;
+        }
+      }
     }
   }
 </style>
